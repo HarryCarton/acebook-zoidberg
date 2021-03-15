f38d03ece070aa7c789e75fd29553b1f1929eb3f
RowHunt <watchlist.rh@gmail.com>
2021-03-15 15:50:15 -0400

    deploy check

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -20,6 +20,9 @@
 
 cd ~
 sudo yum install -y maven
+sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
 sudo yum install -y postgresql postgresql-server
 sudo service postgresql initdb
 sudo service postgresql start
