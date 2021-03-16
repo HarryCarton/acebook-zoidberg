8d98ceb6b248f0d7f23bc06f30beca0511ff9bed
AlistairTait12 <alistair.tait1@gmail.com>
2021-03-16 16:30:57 +0000

    timestamps persisted in the database

--- src/main/java/com/makersacademy/acebook/model/Post.java
+++ src/main/java/com/makersacademy/acebook/model/Post.java
@@ -7,6 +7,11 @@ import javax.persistence.Table;
 import javax.persistence.GenerationType;
 
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @Entity
@@ -18,10 +23,14 @@ public class Post {
     private Long id;
     private String content;
 
+    @CreationTimestamp
+    private Timestamp stamp;
+
     private Post() {}
 
     public Post(String content) {
         this.content = content;
+        this.stamp = Timestamp.valueOf(LocalDateTime.now());
     }
 
 }
