cfee77769a2c8ae94f849f087ed9176ce87e3a06
AlistairTait12 <alistair.tait1@gmail.com>
2021-03-16 13:00:02 +0000

    @EnableWebSecurity added

--- src/main/java/com/makersacademy/acebook/SecurityConfigurer.java
+++ src/main/java/com/makersacademy/acebook/SecurityConfigurer.java
@@ -2,16 +2,17 @@ package com.makersacademy.acebook;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
         auth.inMemoryAuthentication()
-
             .withUser("Zoidberg")
             .password("Zoidypass")
             .roles("USER");
